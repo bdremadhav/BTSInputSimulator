3d8ddfe06ab4d55267dc58837d68c71602e02e2a
madhav <sudam.sagar@wipro.com>
2017-08-31 01:51:30 -0700

    noOfMessagesInFile argument is added

--- src/main/java/com/bts/inputsimulator/BTSInputGenerator.java
+++ src/main/java/com/bts/inputsimulator/BTSInputGenerator.java
@@ -15,11 +15,12 @@ import java.util.TimerTask;
 public class BTSInputGenerator {
     public static void main(String[] args){
         try {
-            String simulatorBaseDir ="/home/ec2-user/simulator/";
+            String simulatorBaseDir ="/home/cloudera/simulator/";
             long scheduleMillis = 1000 * Long.parseLong(args[0]);
             Integer noOfMessagesInBatch = Integer.parseInt(args[1]);
+            int noOfMessagesInFile = Integer.parseInt(args[2]);
 
-            RunTask runTask = new RunTask(noOfMessagesInBatch, simulatorBaseDir);
+            RunTask runTask = new RunTask(noOfMessagesInBatch, simulatorBaseDir,noOfMessagesInFile);
             Timer timer = new Timer();
             timer.scheduleAtFixedRate(runTask, 0, scheduleMillis);
         }catch (Exception e){
@@ -32,10 +33,12 @@ class RunTask extends TimerTask{
     private Integer noOfMessages;
     private Long count;
     private String simulatorBaseDir;
+    private int noOfMessagesInFile;
 
-    public RunTask(Integer noOfMessages, String simulatorBaseDir) throws IOException {
+    public RunTask(Integer noOfMessages, String simulatorBaseDir, int noOfMessagesInFile) throws IOException {
         this.noOfMessages = noOfMessages;
         this.simulatorBaseDir = simulatorBaseDir;
+        this.noOfMessagesInFile = noOfMessagesInFile;
         Scanner scanner = new Scanner(new File(simulatorBaseDir+"bkIdCount.txt"));
         count = scanner.nextLong();
     }
@@ -52,11 +55,14 @@ class RunTask extends TimerTask{
         String[] teColumns = new String[]{"BusinessKey"};
         String[] teColumnValuePrefixes = new String[]{"bk"};
 
-        String dealMsg = "<Deal>         <Header>             <BulkId>MIG01</BulkId>             <ProcModeCd>1</ProcModeCd>                   <BusinessKey>bk1</BusinessKey>                   <SourceTimeStamp>2016-01-25T09:32:01+01:00</SourceTimeStamp>                   <SourceSystemID>S1</SourceSystemID>              </Header>         <Id>id1</Id>             <AccNo>CITINA000000001</AccNo>         <Category>1</Category>               <CtgyCd>1</CtgyCd>         <Status>100</Status>         <ExtTpDesc>DEP</ExtTpDesc>               <Desc>Deal Details</Desc>         <OrderDate>2016-01-25T09:32:01+01:00</OrderDate>         <CreatedAt>2016-01-25T09:32:01+01:00</CreatedAt>         <DealLink>             <Id>TEST20150808000010</Id>         </DealLink>     </Deal>";
-        String tnxMsg = "<Transaction>         <Header>             <BulkId>MIG01</BulkId><!-- used only for migrations -->             <ProcModeCd>1</ProcModeCd><!-- Create --> 			<BusinessKey>bk2</BusinessKey> 			<SourceTimeStamp>2016-01-25T09:34:01+01:00</SourceTimeStamp> 			<SourceSystemID>S1</SourceSystemID>		         </Header>         <Id>id1</Id> 		<CustCode>AMZN</CustCode> <!-- Amazon.com, Inc --> 		<CcyCode>INR</CcyCode> 		<ExchangeCode>NYSE</ExchangeCode> 		<TransType>WEB-BUY</TransType>         <IdScCd>10</IdScCd><!-- P90SOSID -->         <TpCd>1</TpCd><!-- instrumentTransaction -->         <StsCd>1</StsCd><!-- booked --> 		<Status>100</Status>          <DealLink>             <Id>TEST22150808000010</Id>         </DealLink>     </Transaction>";
-        String teMsg = "<TransactionElement>         <Header>             <BulkId>MIG01</BulkId><!-- used only for migrations -->             <ProcModeCd>1</ProcModeCd><!-- Create --> \t\t\t<BusinessKey>bk4</BusinessKey> \t\t\t<SourceTimeStamp>2016-01-25T09:36:01+01:00</SourceTimeStamp> \t\t\t<SourceSystemID>S1</SourceSystemID>\t         </Header>         <Id>id1</Id>         <IdScCd>3</IdScCd><!-- P90SOSIVID -->         <TpCd>3</TpCd><!-- instrumentMovement -->                <BookingTpCd>3</BookingTpCd><!-- book -->         <Qty>500</Qty> \t\t<SettlementAmount>1000000</SettlementAmount> \t\t<SettlementCurrency>INR</SettlementCurrency> \t\t<SettlementCode>101</SettlementCode> <!-- T+2 --> \t\t<CreditDebitFlag>2</CreditDebitFlag><!-- debit -->         <TradeDate>2016-01-25T09:32:01+01:00</TradeDate>         <ValueDate>2016-01-25T09:32:01+01:00</ValueDate>         <BookingDate>2016-01-25T09:32:01+01:00</BookingDate>         <SettlDate>2016-01-25T09:32:01+01:00</SettlDate>         <BookingId>TEST221508087500001000101</BookingId> \t\t<Status>100</Status>         <TrxLink>             <Id>TEST2215080800001000</Id>         </TrxLink>     </TransactionElement>";
+        String dealMsg = "<Deal>         <Header>             <BulkId>MIG01</BulkId>             <ProcModeCd>1</ProcModeCd>                   <BusinessKey>bk1</BusinessKey>                   <SourceTimeStamp>2016-01-25T09:32:01+01:00</SourceTimeStamp>                   <SourceSystemID>S1</SourceSystemID>              </Header>         <Id>id1</Id>             <AccNo>CITINA000000001</AccNo>         <Category>1</Category>               <CtgyCd>1</CtgyCd>         <Status>100</Status>         <ExtTpDesc>DEP</ExtTpDesc>               <Desc>Deal Details</Desc>         <OrderDate>2016-01-25T09:32:01+01:00</OrderDate>         <CreatedAt>2016-01-25T09:32:01+01:00</CreatedAt>         <DealLink>             <Id>TEST20150808000010</Id>         </DealLink>     </Deal>\n";
+        String tnxMsg = "<Transaction>         <Header>             <BulkId>MIG01</BulkId><!-- used only for migrations -->             <ProcModeCd>1</ProcModeCd><!-- Create --> 			<BusinessKey>bk2</BusinessKey> 			<SourceTimeStamp>2016-01-25T09:34:01+01:00</SourceTimeStamp> 			<SourceSystemID>S1</SourceSystemID>		         </Header>         <Id>id1</Id> 		<CustCode>AMZN</CustCode> <!-- Amazon.com, Inc --> 		<CcyCode>INR</CcyCode> 		<ExchangeCode>NYSE</ExchangeCode> 		<TransType>WEB-BUY</TransType>         <IdScCd>10</IdScCd><!-- P90SOSID -->         <TpCd>1</TpCd><!-- instrumentTransaction -->         <StsCd>1</StsCd><!-- booked --> 		<Status>100</Status>          <DealLink>             <Id>TEST22150808000010</Id>         </DealLink>     </Transaction>\n";
+        String teMsg = "<TransactionElement>         <Header>             <BulkId>MIG01</BulkId><!-- used only for migrations -->             <ProcModeCd>1</ProcModeCd><!-- Create --> \t\t\t<BusinessKey>bk4</BusinessKey> \t\t\t<SourceTimeStamp>2016-01-25T09:36:01+01:00</SourceTimeStamp> \t\t\t<SourceSystemID>S1</SourceSystemID>\t         </Header>         <Id>id1</Id>         <IdScCd>3</IdScCd><!-- P90SOSIVID -->         <TpCd>3</TpCd><!-- instrumentMovement -->                <BookingTpCd>3</BookingTpCd><!-- book -->         <Qty>500</Qty> \t\t<SettlementAmount>1000000</SettlementAmount> \t\t<SettlementCurrency>INR</SettlementCurrency> \t\t<SettlementCode>101</SettlementCode> <!-- T+2 --> \t\t<CreditDebitFlag>2</CreditDebitFlag><!-- debit -->         <TradeDate>2016-01-25T09:32:01+01:00</TradeDate>         <ValueDate>2016-01-25T09:32:01+01:00</ValueDate>         <BookingDate>2016-01-25T09:32:01+01:00</BookingDate>         <SettlDate>2016-01-25T09:32:01+01:00</SettlDate>         <BookingId>TEST221508087500001000101</BookingId> \t\t<Status>100</Status>         <TrxLink>             <Id>TEST2215080800001000</Id>         </TrxLink>     </TransactionElement>\n";
 
-        for(int i=1; i<=noOfMessages ; i++) {
+            String dealCombined = "";
+            String tnxCombined = "";
+            String teCombined = "";
+            for(int i=1; i<=noOfMessages ; i++) {
             count++;
             String finalDeal = dealMsg;
             for(int j=0; j<dealColumns.length; j++) {
@@ -79,17 +85,26 @@ class RunTask extends TimerTask{
                 finalTe = finalTe.substring(0,startIndex)+teColumnValuePrefixes[j]+count+finalTe.substring(lastIndex);
             }
 
-                Writer dealWriter = new FileWriter(simulatorBaseDir+"deal/"+count+".xml");
-                dealWriter.write(finalDeal);
-                dealWriter.close();
+                dealCombined = dealCombined + finalDeal;
+                tnxCombined = tnxCombined+finalTnx;
+                teCombined = teCombined + finalTe;
 
-                Writer tnxWriter = new FileWriter(simulatorBaseDir+"tnx/"+count+".xml");
-                tnxWriter.write(finalTnx);
-                tnxWriter.close();
+                if(count % noOfMessagesInFile == 0) {
+                    Writer dealWriter = new FileWriter(simulatorBaseDir + "deal/" + count + ".xml");
+                    dealWriter.write(dealCombined);
+                    dealWriter.close();
+                    dealCombined ="";
 
-                Writer teWriter = new FileWriter(simulatorBaseDir+"te/"+count+".xml");
-                teWriter.write(finalTe);
-                teWriter.close();
+                    Writer tnxWriter = new FileWriter(simulatorBaseDir + "tnx/" + count + ".xml");
+                    tnxWriter.write(tnxCombined);
+                    tnxWriter.close();
+                    tnxCombined = "";
+
+                    Writer teWriter = new FileWriter(simulatorBaseDir + "te/" + count + ".xml");
+                    teWriter.write(teCombined);
+                    teWriter.close();
+                    teCombined="";
+                }
 
         }
 
--- src/main/java/com/bts/inputsimulator/BTSMessageSender.java
+++ src/main/java/com/bts/inputsimulator/BTSMessageSender.java
@@ -15,12 +15,13 @@ import java.util.*;
 public class BTSMessageSender {
     public static void main(String[] args) {
         try {
-            String simulatorBaseDir ="/home/ec2-user/simulator/";
-            String flumeBaseDir = "/home/ec2-user/";
+            String simulatorBaseDir ="/home/cloudera/simulator/";
+            String flumeBaseDir = "/home/cloudera/";
             long scheduleMillis = 1000 * Long.parseLong(args[0]);
             Integer noOfMessagesInBatch = Integer.parseInt(args[1]);
+            int noOfMessagesInFile = Integer.parseInt(args[2]);
 
-            SenderTask senderTask = new SenderTask(noOfMessagesInBatch, simulatorBaseDir, flumeBaseDir);
+            SenderTask senderTask = new SenderTask(noOfMessagesInBatch, simulatorBaseDir, flumeBaseDir,noOfMessagesInFile);
             Timer timer = new Timer();
             timer.scheduleAtFixedRate(senderTask, 0, scheduleMillis);
         }catch (Exception e){
@@ -35,11 +36,13 @@ class SenderTask extends TimerTask{
     private  Long count;
     private String simulatorBaseDir;
     private String flumeBaseDir;
+    private int noOfMessagesInFile;
 
-    public SenderTask(Integer noOfMessages, String simulatorBaseDir, String flumeBaseDir ) throws IOException {
+    public SenderTask(Integer noOfMessages, String simulatorBaseDir, String flumeBaseDir,int noOfMessagesInFile ) throws IOException {
         this.noOfMessages = noOfMessages;
         this.simulatorBaseDir = simulatorBaseDir;
         this.flumeBaseDir = flumeBaseDir;
+        this.noOfMessagesInFile = noOfMessagesInFile;
         Scanner scanner = new Scanner(new File(simulatorBaseDir+"MessagesSentCount.txt"));
         try {
             count = scanner.nextLong();
@@ -50,7 +53,7 @@ class SenderTask extends TimerTask{
                 fileNumbers.add(Long.parseLong(filename.replace(".xml","")));
             }
             Collections.sort(fileNumbers);
-            count = fileNumbers.get(0) -1;
+            count = fileNumbers.get(0) -noOfMessagesInFile;
         }
 
     }
@@ -60,8 +63,9 @@ class SenderTask extends TimerTask{
         System.out.println("Current Time = " + new Date().toString());
         try {
 
-            for(int i=1 ; i<=noOfMessages; i++ ) {
-                Long msgId = ++count;
+            for(int i=1 ; i<=noOfMessages/noOfMessagesInFile; i++ ) {
+                count = count+10;
+                Long msgId = count;
                 Files.move(Paths.get(simulatorBaseDir + "deal/"+ msgId +".xml"), Paths.get(flumeBaseDir + "spool-deal/"+ msgId +".xml"), StandardCopyOption.REPLACE_EXISTING);
                 Files.move(Paths.get(simulatorBaseDir + "tnx/"+ msgId +".xml"), Paths.get(flumeBaseDir + "spool-transaction/"+ msgId +".xml"),StandardCopyOption.REPLACE_EXISTING);
                 Files.move(Paths.get(simulatorBaseDir + "te/"+ msgId +".xml"), Paths.get(flumeBaseDir + "spool-te/"+ msgId +".xml"),StandardCopyOption.REPLACE_EXISTING);
